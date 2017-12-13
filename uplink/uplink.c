10c9e68aaf00b5a6d0511d3014406031d07f3b3e
kneerunjun <kneerunjun@gmail.com>
2017-12-13 01:22:10 +0000

    POST request now working to satisfaction with well formed data

--- uplink/uplink.c
+++ uplink/uplink.c
@@ -13,9 +13,7 @@ struct MemoryStruct {
   char *memory;
   size_t size;
 };
-
-static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
-{
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp){
   size_t realsize = size * nmemb;
   struct MemoryStruct *mem = (struct MemoryStruct *)userp;
   // want to read more about realloc ?
@@ -28,10 +26,58 @@ static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, voi
   }
   memcpy(&(mem->memory[mem->size]), contents, realsize);
   mem->size += realsize;
-  mem->memory[mem->size] = 0;
+  mem->memory[mem->size] = 0; // havent still discovered whats this!
   return realsize;
 }
-int perform_get(char* url){
+
+/*this helps in sending the data to the url as payload
+url   :this is the post url
+paylod : this is the xml/text data that is to be uploaded
+reference :https://curl.haxx.se/libcurl/c/http-post.html
+*/
+long perform_post(char* url, char* payload){
+  // curl -d "param1=value1&param2=value2" -X POST http://localhost:3000/data
+  CURL* curl;
+  CURLcode response = CURLE_OK;
+  long response_code = 0;
+  struct MemoryStruct chunk;
+  chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
+  chunk.size = 0;
+  char error_buf[CURL_ERROR_SIZE];
+  // /* In windows, this will init the winsock stuff */
+  // curl_global_init(CURL_GLOBAL_ALL);
+  curl = curl_easy_init();
+  if(!curl){
+    // is the case when we could not init the curl pointer itself
+    perror("failed to instantiate the CURL client");
+    goto cleanup;
+  }
+  curl_easy_setopt(curl, CURLOPT_URL, url);
+  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+  curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+  // the default bahaviour of CURL is to give CURLE_OK despite non 200 OK return codes
+  // this mandates the curl to emit an error if thhe error from the server is returned
+  curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L); //<= this is important, but not obvious
+  curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, error_buf);
+  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload);
+  response =curl_easy_perform(curl);
+  curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+  if (response!=CURLE_OK) {
+    fprintf(stderr, "%s\n",error_buf);
+    goto cleanup;
+  }
+  printf("CURL POST success , status code %ld\n", response_code);
+  // need to check if the response is in structure
+  printf("We have received %d bytes in the server response \n", chunk.size);
+  printf("%s\n", chunk.memory);
+  cleanup:
+    curl_easy_cleanup(curl);
+    // curl_global_cleanup();
+  return response_code;
+}
+long perform_get(char* url){
   CURL *curl;
   CURLcode res;
   int retCode  = 0;
@@ -39,15 +85,17 @@ int perform_get(char* url){
   chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
   chunk.size = 0;    /* no data at this point */
   curl = curl_easy_init();
+  long response_code =0;
   if(!curl){
     // is the case when we could not init the curl pointer itself
     perror("failed to instantiate the CURL client");
-    retCode = -1;
     goto cleanup;
   }
   curl_easy_setopt(curl, CURLOPT_URL, url);
   curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-  /* send all data to this function  */
+  /* send all data to this function
+  read more on the write function here :
+  https://curl.haxx.se/libcurl/c/CURLOPT_WRITEFUNCTION.html*/
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
   /* we pass our 'chunk' struct to the callback function */
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
@@ -56,7 +104,8 @@ int perform_get(char* url){
   curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
   if(res = curl_easy_perform(curl) != CURLE_OK){
     fprintf(stderr, "We have error response from the server :%d\n", res);
-    retCode =-1;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+    printf("We have response code %ld from the server\n", response_code);
     goto cleanup;
   }
   else {
@@ -66,16 +115,25 @@ int perform_get(char* url){
     *
     * Do something nice with it!
     */
+   curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
    printf("%lu bytes retrieved\n", (long)chunk.size);
    printf("The data we have received is : %s\n", chunk.memory);
+   printf("We have response code %ld from the server\n", response_code);
   }
-  retCode =0;
   cleanup:
     curl_easy_cleanup(curl);
-  return retCode;
+  return response_code;
 }
 int main(int argc, char const *argv[]) {
   char url[]= "http://192.168.1.5:8038/api/uplink/devices/";
   perform_get(url);
+  // the json string if not formatted correctly would result in internal server error 500
+  // observe the escape on the \" quotation marks to make it a valid json string
+  char* deviceDetails = "{\"location\":\"Pune\",\"duty\":\"measure ambient conditions\",\"type\":\"RPi3B\"}";
+  // now we need to post this payload to url and see if this working
+  char* invalidDeviceDetails  = "location=kothrud&duty=measurement+ambientconditions&type=RPi3B";
+  perform_post(url,deviceDetails);
+  // we can then try the same request with invalid payload
+  perform_post(url,invalidDeviceDetails);
   return 0;
 }
