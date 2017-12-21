bd279c5df40155f5da392688a956487e63a7355e
kneerunjun <kneerunjun@gmail.com>
2017-12-21 17:18:20 +0000

    added code to jsonification

--- uplink/uplink.c
+++ uplink/uplink.c
@@ -13,12 +13,11 @@ struct MemoryStruct {
   char *memory;
   size_t size;
 };
-typedef char* (*jsonify)(char*, char*, short);
+typedef char* (*jsonify)(char*, char*);
 typedef struct structkeyvaluepair{
   char* key;
   char* strValue ;
   jsonify Jsonify;
-  short isEdge; //this lets the json parser that a ',' is needed or
 }KeyValuePair;
 /*This defines the KeyValuePair of json values that needs to be read back into a
 json string before being sent across to the server. The client can now still put
@@ -27,26 +26,26 @@ Want to know how to effectively define structures ?
 https://www.programiz.com/c-programming/c-structures
 Want to know how we can store function pointers in structures ?
 http://www.cs.yale.edu/homes/aspnes/pinewiki/C(2f)FunctionPointers.html*/
-char* jsonify_strfield(char* key, char* value, short isEdge){
+char* jsonify_strfield(char* key, char* value){
     //this assumes the field value is char* type and thus converts the same
     char* new_result= malloc(strlen(key)+strlen(value)+10); // initiating to a proper size
     if(!new_result){
       fprintf(stderr, "Out of memory , failed jsonification \n");
       return "";
     }
-    switch (isEdge) {
-      case 0:
-        sprintf(new_result ,"\"%s\":\"%s\",",key, value); // printing on the variable
-        break;
-      case 1:
-        sprintf(new_result ,"\"%s\":\"%s\"}",key, value); // printing on the variable
-        break;
-      case -1:
-        sprintf(new_result ,"{\"%s\":\"%s\",",key, value); // printing on the variable
-        break;
-      default:
-        sprintf(new_result ,"\"%s\":\"%s\",",key, value);
+    // notice that there is , mark after every field
+    sprintf(new_result ,"\"%s\":\"%s\",",key, value);
+    return new_result;
+  }
+char* jsonify_numfield(char* key, char* value){
+    //this assumes the field value is char* type and thus converts the same
+    char* new_result= malloc(strlen(key)+strlen(value)+10); // initiating to a proper size
+    if(!new_result){
+      fprintf(stderr, "Out of memory , failed jsonification \n");
+      return "";
     }
+    // notice that there is , mark after every field
+    sprintf(new_result ,"\"%s\":%s,",key, value);
     return new_result;
   }
 static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp){
@@ -153,32 +152,36 @@ long perform_get(char* url){
     curl_easy_cleanup(curl);
   return response_code;
 }
-/*this iis to test the conversion of structures to jso strings and if all works fine*/
-char* test_jsonification(){
-  KeyValuePair kvLocation={"location","",jsonify_strfield, -1};
-  KeyValuePair kvDuty={"duty","Measure air pollutants",jsonify_strfield, 0};
-  KeyValuePair kvType={"type","RPi3B+ but not sure of the version",jsonify_strfield, 1};
-  KeyValuePair* payload = malloc(3*sizeof(KeyValuePair));
-  *(payload)= kvLocation;
-  *(payload+1) = kvDuty;
-  *(payload+2)= kvType;
-  /*If you are asking what is the point of malloc(0)
-  https://stackoverflow.com/questions/2022335/whats-the-point-in-malloc0*/
-  /*remeber here that malloc(0) does not actually allocate zero bytes as intuitively suggested
-  it would still have a some garbage string - I have tried comapring that to a NULL , empty and even strlen("")
-  nothing works, you have to set that to explicit memset to 0*/
+/*this iis to test the conversion of structures to jso strings and if all works fine
+remeber here that malloc(0) does not actually allocate zero bytes as intuitively suggested
+it would still have a some garbage string - I have tried comapring that to a NULL , empty and even strlen("")
+nothing works, you have to set that to explicit memset to 0
+If you are asking what is the point of malloc(0)
+https://stackoverflow.com/questions/2022335/whats-the-point-in-malloc0*/
+char* test_jsonification(KeyValuePair payload[], unsigned int fields){
   char* json_string  = malloc(0);
   memset(json_string,0,strlen(json_string));
-  size_t i = 0,count = 3;
-  for (i = 0; i < count; i++) {
-    char* appendix = payload->Jsonify(payload->key, payload->strValue, payload->isEdge);
-    json_string = realloc(json_string,strlen(appendix)+1+strlen(json_string));
-    memcpy(json_string+strlen(json_string),appendix,strlen(appendix));
+  size_t i = 0;
+  for (i = 0; i < fields; i++) {
+    // only if we have a valid key
+    if(0!=strcmp(payload->key, "")){
+      char* appendix = payload->Jsonify(payload->key, payload->strValue);
+      json_string = realloc(json_string,strlen(appendix)+1+strlen(json_string));
+      memcpy(json_string+strlen(json_string),appendix,strlen(appendix));
+    }
     payload++;
   }
-  printf("From within the function :\n");
-  printf("%s\n",json_string);
-  return json_string;
+  // here we make that a object from the string that it is
+  char* result = malloc(strlen(json_string)) ;
+  memset(result, 0, strlen(result));
+  memcpy(result, json_string, strlen(json_string)-1);
+  char* json_result = malloc(strlen(result)+2);
+  memset(json_result, 0, strlen(json_result));
+  sprintf(json_result,"{%s}", result);
+  printf("%s\n",json_result);
+  free(json_string);
+  free(result);
+  return json_result;
 }
 
 int test_urlsend(){
@@ -195,9 +198,14 @@ int test_urlsend(){
   return 0;
 }
 int main(int argc, char const *argv[]) {
-  // char* result = test_jsonification();
-  char* json  = test_jsonification();
-  printf("%s\n",json);
+  KeyValuePair payload[] ={
+    {"location","Kothrud Pune 38",jsonify_strfield},
+    {"duty","Measure air pollutants",jsonify_strfield},
+    {"type","RPi3B+ but not sure of the version",jsonify_strfield},
+    {"random","1000",jsonify_numfield}
+  };
+  char* json  = test_jsonification(payload, 4);
+  // printf("%s\n",json);
   // printf("%s\n",result);
   // char url[]= "http://192.168.1.5:8038/api/uplink/devices/";
   // long rcode =perform_post(url,json);
